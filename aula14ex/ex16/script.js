10bfd59507a9e156eef9c1be222bd372aac22b22
Lucas Coura <106277330+lukecoura@users.noreply.github.com>
2022-06-23 22:40:11 -0300

    .

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,14 +1,24 @@
 var inicio = document.querySelector('input#inicio')
 var fim = document.querySelector('input#fim')
 var passo = document.querySelector('input#passo')
+var resul = document.querySelector('div#resul')
 
 function contar() {
     var inicio02 = Number(inicio.value)
     var fim02 = Number(fim.value)
     var passo02 = Number(passo.value)
-    var resul = document.querySelector('div#resul')
+    resul.innerHTML = ('Contando:<br>')
+    if (inicio02 == ('')) {
+        resul.innerHTML('Impossível contar!')
+    }
+
+    if (passo02 == 0) {
+        alert('Passo inválido! Considerando PASSO: 1')
+        passo02++
+    }
     while (inicio02 <= fim02) {
-        resul.innerHTML += (`teste ${inicio02}`)
+        resul.innerHTML += (`${inicio02} 👉 `)
         inicio02 += passo02
     }
+    resul.innerHTML += ('🏁')
 }
