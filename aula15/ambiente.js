74833a9c72ae3f988200563f667ab7c84ff4f392
Lucas Coura <106277330+lukecoura@users.noreply.github.com>
2022-07-01 16:30:03 -0300

    teste

---
+++ aula15/ambiente.js
@@ -0,0 +1,5 @@
+var num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num[1]} posições`)
---
+++ aula15/vetor.js
@@ -0,0 +1,3 @@
+var valores = [8, 1, 7, 4, 2, 9]
+
+var pos = num.indexOf(8)
